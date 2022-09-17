082dde507b53545f58ecccbe1f7338e62afe6b07
Vitória Caroline dos Santos <110695861+vitoriacarolsa@users.noreply.github.com>
2022-09-16 22:42:08 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Music-Player
+<h1>
+<img src="gif.gif">
+</h1>
+
+## 🎶 Sobre 
+
+O projeto **Music Player** é um reprodutor de música, esta atividade foi proposta pelo Professor Gabriel, no **Instituto Proa.**
+
+## 💻 Tecnologias utilizadas 
+
+Tecnologias utilizadas para o desenvolvimento do projeto:
+
+- HTML
+- CSS
+- JAVASCRIPT
+
---
+++ music-list.js
@@ -0,0 +1,89 @@
+let allMusic = [
+    {
+        name: "Tarde De Sol",
+        artist: "Armandinho",
+        img: "music-1",
+        src: "music-1"
+        
+    },
+    {
+        name: "Somebody To Love",
+        artist: "Queen",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Vem Ser Minha",
+        artist: "Charlie Brown Jr",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Penny Lane",
+        artist: "The Beatles",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Forever",
+        artist: "Kiss",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Século XXI",
+        artist: "Raul Seixas",
+        img: "music-6",
+        src: "music-6"
+    },
+
+    {
+        name: "Clearwater Revival",
+        artist: "Creedence",
+        img: "music-7",
+        src: "music-7"
+    },
+
+    {
+        name: "Exagerado",
+        artist: "Cazuza",
+        img: "music-8",
+        src: "music-8"
+    },
+
+    {
+        name: "Mudaram as estações",
+        artist: "Cássia Eller",
+        img: "music-9",
+        src: "music-9"
+    },
+
+    {
+        name: "Always on My Mind",
+        artist: "Elvis Presley",
+        img: "music-10",
+        src: "music-10"
+    },
+
+    {
+        name: "Girls Just Want To Have Fun",
+        artist: "Cyndi Lauper",
+        img: "music-11",
+        src: "music-11"
+    },
+
+    {
+        name: "Infinita Highway",
+        artist: "Engenheiros do Hawaii",
+        img: "music-12",
+        src: "music-12"
+    },
+
+    {
+        name: "Epitáfio",
+        artist: "Titãs",
+        img: "music-13",
+        src: "music-13"
+    },
+    
+    ];
